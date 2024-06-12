2b10809b55b9c087e5617841a453d7a011b4f30c
Babybrawl <120323490+Babybrawl@users.noreply.github.com>
2024-06-12 11:24:58 +0200

    ok

--- js/server.js
+++ js/server.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;  // Utilisez le port de l'environnement si disponible
+const port = 3000;
 
-// Remplacez "votre-uri-mongodb" par l'URI de votre base de données MongoDB
+// MongoDB URI
 const mongoURI = "mongodb+srv://nicolasbabybrawl:QDRGrf8sq2OMtCKH@babybrawl.aod6irz.mongodb.net/?retryWrites=true&w=majority&appName=Babybrawl";
 const client = new MongoClient(mongoURI);
 
-// Middleware pour permettre les requêtes CORS (remplacez '*' par votre domaine si nécessaire)
+// Middleware pour permettre les requêtes CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
+// Servir les fichiers statiques (CSS, JS, images)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Fonction pour connecter MongoDB
 async function connectToMongo() {
   try {
     await client.connect();
-    console.log("Connexion à MongoDB réussie !");
   } catch (error) {
     console.error("Erreur lors de la connexion à MongoDB :", error);
     throw error;
@@ -29,7 +32,6 @@ async function connectToMongo() {
 // Route pour récupérer tous les films
 app.get('/api/movies', async (req, res) => {
   try {
-    // Assurez-vous que la connexion à MongoDB est établie avant de récupérer les films
     await connectToMongo();
 
     const database = client.db("Films_dataBase");
@@ -41,6 +43,11 @@ app.get('/api/movies', async (req, res) => {
     console.error("Erreur lors de la récupération des films :", error);
     res.status(500).json({ error: "Une erreur s'est produite lors de la récupération des films." });
   }
+});
+
+// Route pour servir viewVideo.html
+app.get('/html/viewVideo.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'html', 'viewVideo.html'));
 });
 
 // Écoute du serveur sur le port défini
