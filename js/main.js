2f73bf748043aa8aa3dc80826e8dd094baac0e49
Babybrawl <120323490+Babybrawl@users.noreply.github.com>
2024-06-15 20:28:40 +0200

    zqd

--- js/main.js
+++ js/main.js
@@ -232,6 +232,19 @@ async function start(){
                 console.error('Erreur lors de la récupération des données:', error);
             });
 
+            // Sélectionne la div avec les classes 'jw-video jw-reset'
+            var videoDiv = document.querySelector('.jw-video.jw-reset');
+
+            // Vérifie si l'élément existe avant d'essayer de récupérer sa hauteur
+            if (videoDiv) {
+                // Récupère la hauteur de l'élément
+                var height = videoDiv.offsetHeight;
+                alert('La hauteur actuelle de la div est : ' + height + 'px');
+            } else {
+                alert('L\'élément avec les classes "jw-video jw-reset" n\'a pas été trouvé.');
+            }
+
+
 
     } catch (error) {
         console.error("Une erreur s'est produite dans start2 :", error);
